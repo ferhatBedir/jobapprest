fb6d61e5926571ad45f8c428eea6a1a7d7f6d8e0
ferhatBedir <38124974+ferhatBedir@users.noreply.github.com>
2018-04-18 15:16:45 +0300

    initial commit

---
+++ src/main/java/com/tmod/jobapprest/JobapprestApplication.java
@@ -0,0 +1,12 @@
+package com.tmod.jobapprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobapprestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobapprestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tmod/jobapprest/JobapprestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tmod.jobapprest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JobapprestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
